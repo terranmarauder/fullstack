2b7548cacab4c02c6a3d12d7b5f7003fff77dbae
73598898 <gekkoweb@gmail.com>
2021-01-21 20:50:05 +0200

    part 1 ok

---
+++ part1/part1.1/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>{props.num}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name={course}  />
+
+      <Content part={part1}  />
+      <Total num={exercises1}  />
+
+      <Content part={part2}  />
+      <Total num={exercises2}  />
+      
+      <Content part={part3}  />
+      <Total num={exercises3}  />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
