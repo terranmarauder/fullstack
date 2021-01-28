386c8a820f719b2de96f37b2f6984677e8d8b5ec
73598898 <gekkoweb@gmail.com>
2021-01-28 23:23:46 +0200

    -

--- part1/courseinfo/src/index--1-1.js
+++ part1/courseinfo/src/index--1-1.js
@@ -10,26 +10,22 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-    <Part part={props.part1} exercise={props.exercise1} />
-    <Part part={props.part2} exercise={props.exercise2} />
-    <Part part={props.part3} exercise={props.exercise3} />
+    <p>
+      {props.part1} {props.exercise1}
+    </p>
+    <p>
+      {props.part2} {props.exercise2}
+    </p>
+    <p>
+      {props.part3} {props.exercise3}
+    </p>
     </>
   )
 }
 
-const Part = (props) => {
-	return (
-	<p>
-    {props.part} (props.exercise)
-	</p>	
-	)
-}
-
 const Total = (props) => {
   return (
-    <p>
-      Number of exercise {props.exercise1 + props.exercise2 + props.exercise3}
-    </p>
+    <p>Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}</p>
   )
 }
 
@@ -46,6 +42,7 @@ const App = () => {
     <div>
       <Header name={course} />
       <Content part1={part1} part2={part2} part3={part3} exercise1={exercise1} exercise2={exercise2} exercise3={exercise3} />   
+
       <Total exercise1={exercise1} exercise2={exercise2} exercise3={exercise3} /> 
     </div>
   )
