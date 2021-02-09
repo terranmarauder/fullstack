273dec1a04450d6ac317ad9dfe093135c4eeaa40
73598898 <gekkoweb@gmail.com>
2021-02-09 15:24:58 +0200

    -

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,28 +3,32 @@ import ReactDOM from 'react-dom'
 
 /// consts ///
 
-
+const Good = props => <div>good {props.good}</div>
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 
 /// MAIN APP ///
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setgood] = useState(0)
+  const setTogood = newgood => {
+    setgood(newgood)
+// the two other buttons in here... //
+  
+  }
 
   return (
     <div>
-      code here
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setTogood(good + 1)} text="good" />
+      <h2>Statistics</h2>
+      <Good good={good} />
     </div>
   )
 }
-
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
-
-
 
 /// render in DOM ///
 
